4901647aeef0f82e5f3be74e65f9278f5eb3f4f3
JurgisZ_DesktopPC <jurgi@ZHYMAPC>
2024-09-25 20:52:21 +0300

    Done.

--- CS_EF_Atsiskaitymas_Studentu_Informacine_Sistema/Services/Interfaces/IDepartmentService.cs
+++ CS_EF_Atsiskaitymas_Studentu_Informacine_Sistema/Services/Interfaces/IDepartmentService.cs
@@ -5,8 +5,8 @@ namespace CS_EF_Atsiskaitymas_Studentu_Informacine_Sistema.Services.Interfaces
     public interface IDepartmentService
     {
         int CreateDepartment(Department department);
-        List<Department> GetAll();
-        Department? GetById(int id);
+        public List<Department> GetAll();
+        public Department? GetById(int id);
         public string? GenerateUniqueDepartmentCode(string departmentName);
         public bool IsValidNewDepartmentName(string departmentName);
         public bool IsValidNewDepartmentCode(string? departmentCode);
